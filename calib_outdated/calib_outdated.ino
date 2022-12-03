e3076af95ab2f2580502e2b434c91558d9c2c604
Michael Pan <michaelpan@Michaels-MacBook-Pro.local>
2022-12-02 17:58:14 -0800

    add in old compass calib code

--- calib_outdated/calib_outdated.ino
+++ calib_outdated/calib_outdated.ino
@@ -1,15 +1,15 @@
 #include <Servo.h>
-#include "avoid.h"
+// #include "avoid.h"
 #include "compass.h"
-#include "uv.h"
+// #include "uv.h"
 
 unsigned long totsTime;
 unsigned long oldTime;
 
 void setup() {
-  setup_avoid();
+  // setup_avoid();
   Serial.begin(9600);  //initialized serial port , using Bluetooth as serial port, setting baud
-  stopp();             //stop
+  // stopp();             //stop
   setup_compass();
   delay(1000);
 }
@@ -17,12 +17,12 @@ void loop() {
   // loop_compass();
   // totsTime = millis();
   // oldTime = totsTime;
-  Self_Control();
-  Serial.println("\ncar done");
+  // Self_Control();
+  // Serial.println("\ncar done");
   // Serial.println(millis());
   // Serial.println(oldTime);
   int dir = loop_compass();
   Serial.println("Compass: "+ (String) dir + " degree");
-  float uv = loop_uv();
-  Serial.println("UV: " + (String) uv);
+  // float uv = loop_uv();
+  // Serial.println("UV: " + (String) uv);
 }
--- sunseeker_/uv.h
+++
@@ -1,7 +0,0 @@
-#ifndef UV_H
-#define UV_H
-#include <Arduino.h>
-
-float loop_uv();
-
-#endif
