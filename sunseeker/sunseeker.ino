f30c78766a6c7ce66a5cddd1a5f1732a1e428c67
SwagMasterDave <chwan030@ucr.edu>
2022-12-04 15:51:19 -0800

    seperate bluetooth and add app protocol

--- sunseeker/sunseeker.ino
+++ sunseeker/sunseeker.ino
@@ -2,14 +2,16 @@
 #include "avoid.h"
 #include "compass.h"
 #include "uv.h"
-#include "bluetooth.h"
-
+#define Pin12  12
+#define Pin13  13
 unsigned long totsTime;
 unsigned long oldTime;
+int Value;
 
 void setup() {
   Serial.begin(9600);  //initialized serial port , using Bluetooth as serial port, setting baud
-  setup_bluetooth();
+  pinMode(Pin12, INPUT);
+  pinMode(Pin13, OUTPUT);
   setup_avoid();
   // Serial.begin(9600);  //initialized serial port , using Bluetooth as serial port, setting baud
   stopp();             //stop
@@ -17,20 +19,21 @@ void setup() {
   delay(1000);
 }
 void loop() {
+  Value = digitalRead( Pin12 );
+  if(Value){
+    digitalWrite(Pin13, HIGH );
   // totsTime = millis();
   // oldTime = totsTime;
 
   // *****************************************************************************************
   // Bluetooth communication from user, input desired time window
   // *****************************************************************************************
-  loop_bluetooth();
-  Serial.println("\nBluetooth done");
 
   // *****************************************************************************************
   // Sunseeker starts moving w obstacle avoidance
   // *****************************************************************************************
-  Self_Control();
-  Serial.println("\ncar done");
+    Self_Control();
+    Serial.println("\ncar done");
   // Serial.println(millis());
   // Serial.println(oldTime);
 
@@ -39,19 +42,23 @@ void loop() {
   // *****************************************************************************************
   // float dir = loop_compass();
   // Serial.println("Compass: "+ (String) dir + " degree");
-  float dir_avg = 0.0;
-  for (int i = 0; i < 5; i++) {
-    float dir = loop_compass();
+    float dir_avg = 0.0;
+    for (int i = 0; i < 5; i++) {
+      float dir = loop_compass();
     // Serial.println("Measurement " + (String) (i+1) + ": " + (String) dir + " degree");
-    dir_avg += dir;
-  }
-  Serial.println("Compass: " + (String) (dir_avg/5.0 )+ " degree");
+      dir_avg += dir;
+    }
+    Serial.println("Compass: " + (String) (dir_avg/5.0 )+ " degree");
 
   // *****************************************************************************************
   // Checking if under sunlight
   // *****************************************************************************************
-  float uv = loop_uv();
-  Serial.println("UV: " + (String) uv);
+    float uv = loop_uv();
+    Serial.println("UV: " + (String) uv);
 
-  Serial.println();
+    Serial.println();
+  }
+  else{
+      digitalWrite(Pin13, LOW );
+  }
 }
